4f30099579db9901010c8787c67e6b8ffaf88600
Alfredo Deza <adeza@redhat.com>
2015-01-19 11:05:54 -0500

    create an exceptions module
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

---
+++ radosgw_agent/exceptions.py
@@ -0,0 +1,42 @@
+
+
+class ClientException(Exception):
+    """
+    Base radosgw_agent client exception.
+    """
+    pass
+
+
+class InvalidProtocol(ClientException):
+    pass
+
+
+class InvalidHost(ClientException):
+    pass
+
+
+class InvalidZone(ClientException):
+    pass
+
+
+class ZoneNotFound(ClientException):
+    pass
+
+
+class BucketEmpty(ClientException):
+    pass
+
+
+class HttpError(ClientException):
+    def __init__(self, code, body):
+        self.code = code
+        self.str_code = str(code)
+        self.body = body
+        self.message = 'Http error code %s content %s' % (code, body)
+
+    def __str__(self):
+        return self.message
+
+
+class NotFound(HttpError):
+    pass
