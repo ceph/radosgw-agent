e8b5beebec45155e9f9221a8493a839ddaf2a48b
Alfredo Deza <adeza@redhat.com>
2015-02-02 09:50:02 -0500

    test write ops to be ignored as well
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- radosgw_agent/tests/test_worker.py
+++ radosgw_agent/tests/test_worker.py
@@ -216,7 +216,7 @@ class TestDataWorkerIncremental(object):
         assert len(entries) == 0
 
     @httpretty.activate
-    def test_no_valid_versioned_items_for_delete_op(self):
+    def test_no_valid_versioned_items_for_write_op(self):
         src_body = json.dumps([create_log_entry('foo_1', op='write')])
         self.register(src_body=src_body)
         marker, entries = self.w.get_bucket_instance_entries(2, 'bucket')
