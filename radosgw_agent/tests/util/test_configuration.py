a352a714c710834d08c00b8552cea571dc183423
Alfredo Deza <adeza@redhat.com>
2015-03-11 12:34:43 -0400

    more tests for allowing nested immutable dicts
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- radosgw_agent/tests/util/test_configuration.py
+++ radosgw_agent/tests/util/test_configuration.py
@@ -26,3 +26,25 @@ class TestConfiguration(object):
     def test_keyerror_when_popping(self, conf):
         with pytest.raises(KeyError):
             conf.pop('key')
+
+    def test_adding_nested_values(self, conf):
+        conf['key'] = {}
+        conf['key']['bar'] = 1
+        assert conf['key']['bar'] == 1
+
+    def test_modifiying_nested_values_fails(self, conf):
+        conf['key'] = {}
+        conf['key']['bar'] = 1
+        with pytest.raises(TypeError):
+            conf['key']['bar'] = 2
+
+    def test_initial_dict_seeding(self):
+        my_dict = {'a': 1}
+        conf = configuration.Configuration(my_dict)
+        assert conf['a'] == 1
+
+    def test_initial_dict_seeding_doesnt_allow_updates(self):
+        my_dict = {'a': 1}
+        conf = configuration.Configuration(my_dict)
+        with pytest.raises(TypeError):
+            conf['a'] = 2
