9eb661edb3bd8234f9a85f7849e0230c514636ed
Alfredo Deza <adeza@redhat.com>
2015-03-11 10:00:44 -0400

    contain the dict object instead of inheriting
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- radosgw_agent/util/configuration.py
+++ radosgw_agent/util/configuration.py
@@ -1,10 +1,16 @@
 
 
-class Configuration(dict):
+class Configuration(object):
     """
     An immutable dictionary where values set for the first time are allowed and
     existing keys raise a TypeError exception
     """
+
+    def __init__(self):
+        self._dict = {}
+
+    def __str__(self):
+        return self._dict
 
     def pop(self, key, default=None):
         self._default_error()
@@ -18,14 +24,20 @@ class Configuration(dict):
     def clear(self):
         self._default_error()
 
+    def get(self, key, default=None):
+        return self._dict.get(key, default)
+
     def _default_error(self):
         msg = 'config object does not allow key changes'
         raise TypeError(msg)
 
     def __setitem__(self, key, value):
         try:
-            self[key]
+            self._dict[key]
         except KeyError:
-            self[key] = value
+            self._dict[key] = value
         else:
             self._default_error()
+
+    def __getitem__(self, key):
+        return self._dict[key]
